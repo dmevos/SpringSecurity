7f3f0bc9b47f9a980ce2cd266b207f0f251da04d
Dmitry Osipov <deosipov@mail.ru>
2023-03-24 00:00:17 +0300

    Method security

--- src/main/java/ru/osipov/springsecurity/controller/MyController.java
+++ src/main/java/ru/osipov/springsecurity/controller/MyController.java
@@ -1,29 +1,71 @@
 package ru.osipov.springsecurity.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
+
 @RestController
-public class MyController{
+public class MyController {
 
-    @GetMapping("all")
+    final String LINKS = "<br><br><a href=http://localhost:8080>Доступ всем</a>" +
+            "<br><br><a href=http://localhost:8080/onlyAdminRead>Только Админиы и Чтение</a>" +
+            "<br><br><a href=http://localhost:8080/onlyAdminWrite>Только Админиы и Запись</a>" +
+            "<br><br><a href=http://localhost:8080/onlyUserDelete>Только Юзеры и Удаляторы</a>" +
+            "<br><br><a href=http://localhost:8080/logout>Логаут</a>";
+
+
+    @GetMapping()
     public String getAll() {
-        return "Hello everyone!";
+        return "Привет всем!" + LINKS;
     }
 
-    @GetMapping("onlyAuth")
-    public String getOnlyAuth() {
-        return "only Auth!";
+    @GetMapping("onlyAdminRead")
+    @Secured({"ROLE_ADMIN", "ROLE_READ"})
+    public String getOnlyAdminRead() {
+        var username = SecurityContextHolder.getContext().getAuthentication().getName();
+        var rights = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        return "Привет <b>" + username + "</b> ты имеешь роли: " + rights +
+                "<br>метод имеет права: Only ADMIN and READ!" + LINKS +
+                "<br><br><a href=http://localhost:8080/another?username=" + username + ">Запустить еще один метод под своим именем</a>" +
+                "<br><a href=http://localhost:8080/another?username=Glafira>Запустить еще один метод под именем \"Glafira\"</a>";
     }
 
-    @GetMapping("onlyRead")
-    public String getOnlyRead() {
+    @GetMapping("onlyAdminWrite")
+    @RolesAllowed({"ROLE_ADMIN", "ROLE_WRITE"})
+    public String getOnlyAdminWrite() {
+        var username = SecurityContextHolder.getContext().getAuthentication().getName();
+        var rights = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        return "Привет <b>" + username + "</b> ты имеешь роли: " + rights +
+                "<br>метод имеет права: Only ADMIN and WRITE!" + LINKS +
+                "<br><br><a href=http://localhost:8080/another?username=" + username + ">Запустить еще один метод под своим именем</a>" +
+                "<br><a href=http://localhost:8080/another?username=Glafira>Запустить еще один метод под именем \"Glafira\"</a>";
 
-        return "only Read!";
     }
-    @GetMapping("onlyWrite")
-    public String getOnlyWrite() {
 
-        return "only Write!";
+    @GetMapping("onlyUserDelete")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_DELETE')")
+    public String getOnlyUsersDelete() {
+        var username = SecurityContextHolder.getContext().getAuthentication().getName();
+        var rights = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        return "Привет <b>" + username + "</b> ты имеешь роли: " + rights +
+                "<br>метод имеет права: Only USER and DELETE!" + LINKS +
+                "<br><br><a href=http://localhost:8080/another?username=" + username + ">Запустить еще один метод под своим именем</a>" +
+                "<br><a href=http://localhost:8080/another?username=Glafira>Запустить еще один метод под именем \"Glafira\"</a>";
+    }
+
+    @GetMapping("another")
+    @PreAuthorize("#username == authentication.principal.username")
+    public String getAnother(@RequestParam String username) {
+        var uname = SecurityContextHolder.getContext().getAuthentication().getName();
+        var rights = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        return "Привет <b>" + username + "</b> ты имеешь роли: " + rights +
+                "<br>метод имеет права: Only " + uname + LINKS +
+                "<br><br><a href=http://localhost:8080/another?username=" + username + ">Запустить еще один метод под своим именем</a>" +
+                "<br><a href=http://localhost:8080/another?username=Glafira>Запустить еще один метод под именем \"Glafira\"</a>";
     }
 }
